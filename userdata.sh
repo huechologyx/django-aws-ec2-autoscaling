b77d747a33577c4bc436994dd48bfbdd9f9d8fcb
huechologyx <hu.echologyx@gmail.com>
2024-08-13 15:52:31 +0600

    added code deploy agent

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 set -e
 
+
+
 # Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
 GIT_REPO_URL="https://github.com/huechologyx/django-aws-ec2-autoscaling.git"
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
 PROJECT_MAIN_DIR_NAME="django-aws-ec2-autoscaling"
+
+##########
+sudo apt-get install wget -y
+cd /home/ubuntu
+wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+
+#########
 
 # Clone repository
 git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"

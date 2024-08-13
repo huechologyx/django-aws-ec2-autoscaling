a3ba88551146fe93a3f2a64737c86e696e781b7b
huechologyx <hu.echologyx@gmail.com>
2024-08-13 18:40:27 +0600

    modified index file

--- userdata.sh
+++ userdata.sh
@@ -10,14 +10,20 @@ GIT_REPO_URL="https://github.com/huechologyx/django-aws-ec2-autoscaling.git"
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
 PROJECT_MAIN_DIR_NAME="django-aws-ec2-autoscaling"
 
-##########
+# install wget
+sudo apt-get update -y 
+sudo apt-get install ruby -y
 sudo apt-get install wget -y
 cd /home/ubuntu
+
+sudo apt update && sudo apt upgrade
+
+
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 
-#########
+
 
 # Clone repository
 git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
